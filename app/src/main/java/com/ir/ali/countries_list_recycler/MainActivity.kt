9c6f834af4aaf889eff6588982373fe23b3b1e7a
Ali <biz.alihabibi@gmail.com>
2024-09-11 13:09:04 +0330

    Qualification ArrayList of Countries

--- app/src/main/java/com/ir/ali/countries_list_recycler/MainActivity.kt
+++ app/src/main/java/com/ir/ali/countries_list_recycler/MainActivity.kt
@@ -12,11 +12,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-
         val countries: ArrayList<CountriesData> =
             arrayListOf(
-
+                CountriesData("USA", "North America", "+1", R.drawable.usa_flag),
+                CountriesData("Argentina", "South America", "+54", R.drawable.argentina_falg),
+                CountriesData("Mexico", "North America", "+52", R.drawable.mexcio_flag),
+                CountriesData("Canada", "North America", "+1", R.drawable.canada_flag),
+                CountriesData("Brazil", "South America", "+55", R.drawable.brazile_flag),
+                CountriesData("Finland", "Europe", "+358", R.drawable.finland_flag),
+                CountriesData("Russia", "Asia", "+7", R.drawable.russia_falg),
+                CountriesData("France", "Europe", "+33", R.drawable.france_flag),
+                CountriesData("China", "Asia", "+86", R.drawable.china_flag),
+                CountriesData("Germany", "Europe", "+49", R.drawable.germany_flag),
+                CountriesData("United Kingdom", "Europe", "+44", R.drawable.uk_flag),
+                CountriesData("Philippines", "Asia", "+63", R.drawable.philippine_flag),
+                CountriesData("Japan", "Asia", "+81", R.drawable.japan_flag),
+                CountriesData("Spain", "Europe", "+34", R.drawable.spain_flag)
             )
         binding.RecyclerView.layoutManager =
             LinearLayoutManager(this, RecyclerView.VERTICAL, false)
