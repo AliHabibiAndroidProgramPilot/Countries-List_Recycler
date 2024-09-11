14c93f709734162d2a4feea831cbd8d6ab42290a
Ali <biz.alihabibi@gmail.com>
2024-09-11 11:21:12 +0330

    Add Countries Data Class

---
+++ app/src/main/java/com/ir/ali/countries_list_recycler/CountriesData.kt
@@ -0,0 +1,9 @@
+package com.ir.ali.countries_list_recycler
+
+
+data class CountriesData(
+    var countryName: String,
+    var countryContinent: String,
+    var countryCode: String,
+    var countryFlag: Int
+)
